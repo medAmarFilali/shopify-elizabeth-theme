08be526c8f4b7bb5dbd1d0adf9587ff52d745a99
Filali Med Amar <medamarfilali@gmail.com>
2021-12-21 13:02:28 +0100

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "./layout/**/*.{html,js,liquid}",
+    "./sections/**/*.{html,js,liquid}",
+    "./templates/**/*.{html,js,liquid}",
+    "./snippets/**/*.{html,js,liquid}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
